aba5079128c98ee6b12bce4655178de52ce6c876
Aathira13 <koaathira@gmail.com>
2021-10-19 01:22:56 +0530

    create NeonNumber.cpp

---
+++ NeonNumber.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+ 
+int checkNeon(int x)
+{
+  int sq = x * x;
+  int sum_digits = 0;
+  while (sq != 0) {
+      sum_digits = sum_digits + sq % 10;
+      sq = sq / 10;
+  }
+  return (sum_digits == x);
+}
+
+int main(void)
+{
+  for (int i = 1; i <= 10000; i++)
+      if (checkNeon(i))
+          cout << i << " ";   
+  return 0;
+}
